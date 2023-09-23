d4f1e3141dae3bbab32d0917a97fc581af487820
atouba <anasstouba5@gmail.com>
2023-09-23 16:01:12 +0100

    alter command message format (prefix)

--- command/send.cpp
+++ command/send.cpp
@@ -11,8 +11,12 @@
 
 void	command_message(ircserv& serv, int client_s, std::string command,
 						std::string param) {
+// 	std::string	msg = "\r\n:" + serv._clients[client_s]->get_nickname() + 
+// 						" " + command + " " + param + "\r\n";
 	std::string	msg = "\r\n:" + serv._clients[client_s]->get_nickname() + \
-						" " + command + " " + param + "\r\n";
+						"!" + serv._clients[client_s]->get_username() + \
+						"@localhost " + command + " " + param + "\r\n";
+	std::cout << "command_message: {" << msg << "}" << std::endl;
 
 	if (send(client_s, msg.c_str(), msg.size() + 1, 0) < 0)
 		std::cout << "command " + command + " message didn't get sent\n";
@@ -26,6 +30,8 @@ void	numerical_message(ircserv& serv, int client_s, int num,
 	std::string	msg = "\r\n:localhost " + num_s + " " + \
 					serv._clients[client_s]->get_nickname() + " " + \
 					param + "\r\n";
+
+	std::cout << "numerical_message: {" << msg << "}" << std::endl;
 
 	if (send(client_s, msg.c_str(), msg.size() + 1, 0) < 0)
 		std::cout << "numeric " + num_s + " message didn't get sent\n";
