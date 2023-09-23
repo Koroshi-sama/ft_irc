de93bb4b8db4e119659478625d6ef1e2fa80ee84
atouba <anasstouba5@gmail.com>
2023-09-23 18:37:54 +0100

    invite mode

--- command/MODE.cpp
+++ command/MODE.cpp
@@ -6,21 +6,54 @@
 /*   By: aerrazik <aerrazik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 18:30:13 by aerrazik          #+#    #+#             */
-/*   Updated: 2023/09/23 14:08:35 by atouba           ###   ########.fr       */
+/*   Updated: 2023/09/23 18:34:20 by atouba           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "command.hpp"
 
-void	mode_invite_only() {}
+void	mode_invite_only(ircserv& serv, std::string chan, int client_s,
+							char action) {
+	std::string	msg;
+	bool		value = (action == '+') ? true : false;
 
-void	mode_topic() {}
+	serv._channels[chan]->set_invite_bool(value);
+// 	command_message(serv, client_s, "MODE", chan + " " + action + "i");
+
+	msg = "\r\n:" + serv._clients[client_s]->get_nickname() +
+		  "!" + serv._clients[client_s]->get_username() +
+		  "@localhost MODE " + chan + " " + action + "i\r\n";
+	forward_to_chan(serv, chan, msg, client_s, true);
+	std::cout << "Mode Invite Only function\n";
+}
+
+void	mode_topic() {
+	
+}
 
 void	mode_key() {}
 
 void	mode_op_privileges() {}
 
 void	mode_user_limit() {}
+
+bool	check_mode_req(ircserv& serv, std::string chan, int client_s) {
+	if (serv._channels.find(chan) == serv._channels.end())
+		return (
+			numerical_message(serv, client_s, 403,
+								chan + " :No such chan..."),
+			false
+			);
+	// checks client is operator
+	if (!client_in_chan(serv, chan, socket_nick(serv, client_s),
+						0))
+		return (
+			numerical_message(serv, client_s, 482,
+							chan + " :No chan op..."),
+			false
+			);
+	return true;
+}
 
 void Command::mode(std::vector<std::string> &vc, int client_socket) {
 // ----------------------------------------------------------------
@@ -35,13 +68,18 @@ void Command::mode(std::vector<std::string> &vc, int client_socket) {
 
 // ---------------------------------------------------------------
 
+	if (vc[2].size() != 2)
+		std::cout << "mode chars are Missing!!!!\n"; 
     char	mode = vc[2][1];        // could be i | l...
-// 	char	action = vc[2][0];      // could be - | + 
+	char	action = vc[2][0];      // could be - | + 
 
-	(void)client_socket;
+	if (!check_mode_req(*_ircserv, vc[1], client_socket)) {
+		std::cout << "Error in mode request!!!!\n";
+		return ;
+	}
 	switch (mode) {
 		case ('i'):
-			mode_invite_only();
+			mode_invite_only(*_ircserv, vc[1], client_socket, action);
 			break ;
 		case ('t'):
 			mode_topic();
