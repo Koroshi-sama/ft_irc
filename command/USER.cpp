efc25ad82ac51dbc96972cc65262610e2c110f91
Ayoub Erraziki <aerrazik@c3r7p8.1337.ma>
2023-09-25 13:57:50 +0100

    done

--- command/USER.cpp
+++ command/USER.cpp
@@ -6,7 +6,7 @@
 /*   By: aerrazik <aerrazik@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 17:55:22 by aerrazik          #+#    #+#             */
-/*   Updated: 2023/09/25 12:27:35 by aerrazik         ###   ########.fr       */
+/*   Updated: 2023/09/25 13:56:31 by aerrazik         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,21 +27,21 @@ void Command::user(std::vector<std::string> &vc, int client_socket) {
         }
         client->set_username(vc[1]);
         client->set_hostname(vc[3]);
-        client->set_realname(vc[4].substr(1) + " " + vc[5]);
+        client->set_realname(vc[4].substr(1));
         // Macro that means user is registred
         client->set_check_user(USERCHECKED);
         std::cout << "[" << client->get_nickname() << "]" << std::endl;
         std::string reply = "001 RPL_WELCOME\r\nWelcome to the IRC Network " + client->get_nickname() + "!"+ client->get_username() + "@" + client->get_hostname() + "\r\n";
-        send(client_socket, reply.c_str(), reply.size(), 0);
+        send(client_socket, reply.c_str(), reply.size() + 1, 0);
         reply = "002 RPL_YOURHOST\r\nYour host is " + client->get_hostname() + ", running version 1.0\r\n";
-        send(client_socket, reply.c_str(), reply.size(), 0);
+        send(client_socket, reply.c_str(), reply.size() + 1, 0);
         reply = "003 RPL_CREATED\r\nThis server was created " + _ircserv->get_creation_time() + "\r\n";
-        send(client_socket, reply.c_str(), reply.size(), 0);
+        send(client_socket, reply.c_str(), reply.size() + 1, 0);
         reply = "004 RPL_MYINFO\r\nIRC 1.0 +itkol\r\n:RPL_WELCOME!" + client->get_username() + "@" + client->get_hostname() + " NICK " + client->get_nickname() + "\r\n";
-        send(client_socket, reply.c_str(), reply.size(), 0);
+        send(client_socket, reply.c_str(), reply.size() + 1, 0);
         std::cout << "Wach tsifet" << std::endl;
         reply = "005  RPL_ISUPPORT\r\n" + client->get_nickname() + " CHANTYPES=# CHANLIMIT=#:1\r\n";
-        send(client_socket, reply.c_str(), reply.size(), 0);
+        send(client_socket, reply.c_str(), reply.size() + 1, 0);
         client->set_status(ONLINE);
     }
 }
